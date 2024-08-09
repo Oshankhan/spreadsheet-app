3a6599fd2642382b2a147e8be93894474dc06c43
Oshankhan <oshankhan07@gmail.com>
2024-08-09 14:12:13 +0530

    Tool bar

--- app/component/Toolbar.js
+++ app/component/Toolbar.js
@@ -2,16 +2,28 @@
 import React from "react";
 import { useStore } from "../store/store";
 import { GoNumber } from "react-icons/go";
-import { FaAlignLeft, FaAlignRight } from "react-icons/fa";
+import { FaAlignLeft, FaAlignRight, FaRedo, FaUndo } from "react-icons/fa";
 import { RiFontSize2 } from "react-icons/ri";
 import { FaAlignCenter } from "react-icons/fa6";
 import { ImFileText2, ImFontSize } from "react-icons/im";
 import { RxFontRoman } from "react-icons/rx";
 const Toolbar = ({ selectedCell, applyValidation }) => {
   const setFormat = useStore((state) => state.setFormat);
+  const undo = useStore((state) => state.undo);
+  const redo = useStore((state) => state.redo);
+  const history = useStore((state) => state.history);
+  const future = useStore((state) => state.future);
+
+  console.log(future);
 
   return (
     <div className="flex w-[80%] h-8 my-2 justify-evenly rounded-md  bg-white">
+      <button onClick={undo} disabled={history.length === 0}>
+        <FaUndo className={`${history.length === 0 && "opacity-50"}`} />
+      </button>
+      <button onClick={redo} disabled={future.length === 0}>
+        <FaRedo className={`${future.length === 0 && "opacity-50"}`} />
+      </button>
       <button onClick={() => applyValidation("number")}>
         <GoNumber />
       </button>
--- app/store/store.js
+++ app/store/store.js
@@ -11,12 +11,45 @@ export const useStore = create((set) => ({
   }),
   currentPage: 1,
   pageSize: 100,
+  history: [],
+  future: [],
 
   updateCell: (index, value) =>
     set((state) => {
       const newCells = [...state.cells];
+      const prevState = [...state.history];
       newCells[index] = { ...newCells[index], value };
-      return { cells: newCells };
+      return {
+        cells: newCells,
+        history: [...prevState, state.cells],
+        future: [],
+      };
+    }),
+  undo: () =>
+    set((state) => {
+      if (state.history.length > 0) {
+        const prevState = [...state.history];
+        const currentCells = state.cells;
+        const lastState = prevState.pop();
+        return {
+          cells: lastState,
+          history: prevState,
+          future: [...state.future, currentCells],
+        };
+      }
+    }),
+  redo: () =>
+    set((state) => {
+      if (state.future.length > 0) {
+        const nextState = [...state.future];
+        const currentCells = state.cells;
+        const lastState = nextState.pop();
+        return {
+          cells: lastState,
+          history: [...state.history, currentCells],
+          future: nextState,
+        };
+      }
     }),
   setValidation: (index, validation) =>
     set((state) => {
